075fb8a71c9944d299be15075f0aa822ec9119c6
Skye Krzykwa <krzyk@umich.edu>
2024-04-03 12:09:35 -0400

    added cmd args for text only, changed model to codey

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import speech_recognition as sr
 import vertexai
 from vertexai.generative_models import GenerativeModel, ChatSession
+from vertexai.language_models import CodeChatModel
 from google.cloud import texttospeech
 import pyaudio
+import argparse
 
 PROJECT_ID = "duck-414417"
 GOOGLE_CLOUD_SPEECH_CREDENTIALS = "./creds/speechtotext.json"
 
-AI_MODEL = "gemini-1.0-pro"
+AI_MODEL = "codechat-bison@002"
+LOCATION = "us-central1"
 VOICE_MODEL = "en-US-Journey-F"
 FRAME_SIZE = 4096
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('-t', '--text', action='store_true')
+
+args = parser.parse_args()
 
 def speech_to_text():
     print("Recognizing speech...")
@@ -28,17 +38,14 @@ def speech_to_text():
 
 def llm_respond(model, input_text):
     print("Using LLM to generate response...")
-    print("Input: " + input_text)
+    if args.verbose: print("Input: " + input_text)
     text_response = []
-    responses = chat.send_message(input_text, stream=True)
-    for chunk in responses:
-        text_response.append(chunk.text)
-    return "".join(text_response)
+    response = chat.send_message(input_text)
     return response.text
 
 def text_to_speech(client, output, input_text):
     print("Speaking...")
-    print("Output: " + input_text)
+    if args.verbose: print("Output: " + input_text)
     voice = texttospeech.VoiceSelectionParams(
         name=VOICE_MODEL,
         language_code="en-US"
@@ -57,8 +64,9 @@ def text_to_speech(client, output, input_text):
 with open('prompt.txt', 'r') as file:
     prompt = file.read()
 
-vertexai.init(project=PROJECT_ID, location="us-central1")
-model = GenerativeModel(AI_MODEL)
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+#model = GenerativeModel(AI_MODEL)
+model = CodeChatModel.from_pretrained(AI_MODEL)
 chat = model.start_chat()
 chat.send_message(prompt)
 
@@ -68,6 +76,10 @@ output = p.open(format=pyaudio.paInt16, channels=1, rate=44100, output=True, fra
 client = texttospeech.TextToSpeechClient()
 
 while True:
-    text = speech_to_text()
+    if args.text: text = input("Input: ")
+    else: text = speech_to_text()
+    
     response = llm_respond(chat, text)
-    text_to_speech(client, output, response)
+
+    if args.text: print("Output: " + response)
+    else: text_to_speech(client, output, response)
