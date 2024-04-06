eeca6c4500ef23b98ea997a40f77b01cedbe3160
Skye Krzykwa <krzyk@umich.edu>
2024-04-06 17:23:02 -0400

    embodiment

--- embodiment.py
+++ embodiment.py
@@ -1,11 +1,13 @@
 from piservo import Servo
+import time
 
 class Embodiment:
     def __init__(self):
-        self.leftWing = Servo(0)
-        self.rightWing = Servo(1)
-        self.headPitch = Servo(2)
-        self.headYaw = Servo(3)
+        self.leftWing = Servo(19)
+        self.rightWing = Servo(13)
+        self.headPitch = Servo(12)
+        self.headYaw = Servo(18)
+        self.toNeutral
     
     def toNeutral(self):
         self.setLeftWing(0)
@@ -14,13 +16,58 @@ class Embodiment:
         self.setHeadYaw(0)
 
     def setLeftWing(self, degrees):
-        self.leftWing.write(180 - degrees)
+        if not (-25 <= degrees <= 155):
+            print("Invalid angle! Range limited to [-25, 155]")
+            return
+        self.leftWingDegrees = degrees
+        self.leftWing.write(self.leftWingDegrees+25)
 
-    def setRightWing(self, degrees):
-        self.rightWing.write(degrees)
+    def setRightWing(self, degrees): # down to -25, up to 155
+        if not (-25 <= degrees <= 155):
+            print("Invalid angle! Range limited to [-25, 155]")
+            return
+        self.rightWingDegrees = degrees
+        self.rightWing.write(180 - (self.rightWingDegrees+25))
 
     def setHeadPitch(self, degrees):
-        self.headPitch.write(degrees + 90)
+        if not (-5 <= degrees <= 25):
+            print("Invalid angle! Range limited to [-5, 25]")
+            return
+        self.headPitchDegrees = degrees
+        self.headPitch.write(180 - self.headPitchDegrees - 90)
 
     def setHeadYaw(self, degrees):
-        self.headYaw.write(degrees + 90)
+        if not (-90 <= degrees <= 90):
+            print("Invalid angle! Range limited to [-90, 90]")
+            return
+        self.headYawDegrees = degrees
+        self.headYaw.write(180 - self.headYawDegrees - 90)
+
+    def getLeftWing(self): return self.leftWingDegrees
+    def getRightWing(self): return self.rightWingDegrees
+    def getHeadPitch(self): return self.headPitchDegrees
+    def getHeadYaw(self): return self.headYawDegrees
+
+if __name__ == "__main__":
+    duck = Embodiment()
+    duck.toNeutral()
+    cmd = ""
+    while cmd != "q":
+        if cmd != "":
+            args = cmd.split()
+            if len(args) == 2:
+                args[1] = int(args[1])
+                if args[0] == "l":
+                    duck.setLeftWing(args[1])
+                elif args[0] == "r":
+                    duck.setRightWing(args[1])
+                elif args[0] == "p":
+                    duck.setHeadPitch(args[1])
+                elif args[0] == "y":
+                    duck.setHeadYaw(args[1])
+                else:
+                    print(f"Invalid command: {cmd}")
+            else:
+                print(f"Invalid command: {cmd}")
+        print(f"l: {duck.getLeftWing()}, r: {duck.getRightWing()}, p: {duck.getHeadPitch()}, y: {duck.getHeadYaw()}")
+        cmd = input()
