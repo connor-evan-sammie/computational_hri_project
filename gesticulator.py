225f8950c119291b97b83afefc09a25d16c5d45b
Connor Williams <willcon@UMICH.EDU>
2024-04-06 11:07:19 -0400

    changed print gesture behaviors

--- gesticulator.py
+++ gesticulator.py
@@ -5,33 +5,39 @@ def who():
     for c in phrase:
         print(c, end="", flush="True")
         time.sleep(0.1)
+    print()
 
 def what():
     phrase = "the What(tm) gesture"
     for c in phrase:
         print(c, end="", flush="True")
         time.sleep(0.1)
+    print()
 
 def when():
     phrase = "the When(tm) gesture"
     for c in phrase:
         print(c, end="", flush="True")
         time.sleep(0.1)
+    print()
 
 def where():
     phrase = "the Where(tm) gesture"
     for c in phrase:
         print(c, end="", flush="True")
         time.sleep(0.1)
+    print()
 
 def why():
     phrase = "the Why(tm) gesture"
     for c in phrase:
         print(c, end="", flush="True")
         time.sleep(0.1)
+    print()
 
 def how():
     phrase = "the How(tm) gesture"
     for c in phrase:
         print(c, end="", flush="True")
         time.sleep(0.1)
+    print()
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_CLOUD_CREDENTIALS
 
 AI_MODEL = "codechat-bison@002"
 LOCATION = "us-central1"
-VOICE_MODEL = "en-US-Journey-F"
+VOICE_MODEL = "en-US-Casual-K" #"en-US-Journey-F"
 FRAME_SIZE = 4096
 
 parser = argparse.ArgumentParser()
@@ -88,11 +88,11 @@ def backchannel_callback():
     i = random.randint(0, len(gestures)-1)
     gh.addToQueue(gestures[i])
 
+text_to_speech(client, output, "quack!")
 gh.start()
 while True:
     if args.text: text = input("Input: ")
     else:
-        text_to_speech(client, output, "quack!")
         bd.start(backchannel_callback)
         text = speech_to_text()
         bd.stop()
