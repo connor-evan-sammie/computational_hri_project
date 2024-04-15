d19883649d876873646c71a1eefe6216a2c8f6ba
Connor Williams <willcon@UMICH.EDU>
2024-04-15 15:50:03 -0400

    Added print statements to test file

--- evaluation_code/buggy_code1_tests.py
+++ evaluation_code/buggy_code1_tests.py
@@ -3,12 +3,17 @@ from buggy_code1 import LinkedList
 """ vvv TEST CASES - DO NOT MODIFY vvv """
 
 llist = LinkedList()
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 
 llist.insertAtBegin(3)
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == [3]
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 1
+print(f"List at idx 0: {llist.at(0)}")
 assert llist.at(0) == 3
 index = False
 try:
@@ -17,7 +22,9 @@ except:
     index = True
 assert index
 llist.removeFirstNode()
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
@@ -27,8 +34,11 @@ except:
 assert index
 
 llist.insertAtEnd(3)
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == [3]
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 1
+print(f"List at idx 0: {llist.at(0)}")
 assert llist.at(0) == 3
 index = False
 try:
@@ -37,7 +47,9 @@ except:
     index = True
 assert index
 llist.removeLastNode()
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
@@ -47,8 +59,11 @@ except:
 assert index
 
 llist.insertAtIndex(3, 0)
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == [3]
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 1
+print(f"List at idx 0: {llist.at(0)}")
 assert llist.at(0) == 3
 index = False
 try:
@@ -57,7 +72,9 @@ except:
     index = True
 assert index
 llist.removeAtIndex(0)
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
@@ -68,7 +85,9 @@ assert index
 
 llist.insertAtBegin(3)
 llist.removeNode(3)
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
@@ -79,14 +98,20 @@ assert index
 
 for i in range(10):
     llist.insertAtBegin(i)
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 10
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 for i in range(10):
+    print(f"List at idx {i}: {llist.at(i)}")
     assert llist.at(i) == 10-i-1
 for i in range(10):
+    print(f"List at idx {llist.at(llist.sizeOfLL() - 1)}: {llist.at(llist.sizeOfLL() - 1)}")
     assert llist.at(llist.sizeOfLL() - 1) == i
     llist.removeLastNode()
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
@@ -97,14 +122,20 @@ assert index
 
 for i in range(10):
     llist.insertAtEnd(i)
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 10
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 for i in range(10):
+    print(f"List at idx {i}: {llist.at(i)}")
     assert llist.at(i) == i
 for i in range(10):
+    print(f"List at idx 0: {llist.at(0)}")
     assert llist.at(0) == i
     llist.removeFirstNode()
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
@@ -115,14 +146,20 @@ assert index
 
 for i in range(10):
     llist.insertAtIndex(i, i)
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 10
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 for i in range(10):
+    print(f"List at idx {i}: {llist.at(i)}")
     assert llist.at(i) == i
 for i in range(10):
+    print(f"List at idx 0: {llist.at(0)}")
     assert llist.at(0) == i
     llist.removeAtIndex(0)
+print(f"Current list: {llist.getAsPythonList()}")
 assert llist.getAsPythonList() == []
+print(f"Size: {llist.sizeOfLL()}")
 assert llist.sizeOfLL() == 0
 index = False
 try:
